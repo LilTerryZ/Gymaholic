61ac15f6dec38e13839d89b549d5ad7102c0bbb8
Muhammad Usman Shahid <shahid2@sheridanc.on.ca>
2021-03-27 21:57:18 -0400

    done?

---
+++ app/src/main/java/com/example/gymaholic/ScheduleFragment.kt
@@ -0,0 +1,25 @@
+package com.example.gymaholic
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class ScheduleFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.schedule_fragment, container, false)
+        // Inflate the layout for this fragment
+        return view
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance() =
+                ScheduleFragment()
+    }
+}
